43e4a1163606c77611cc36010eab251b71facc92
Aryan Nagar <40473422+aryannagar@users.noreply.github.com>
2024-03-22 11:58:51 +0000

    String.startsWith()

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const str = 'The quick brown fox jumps over the lazy dog.';
-
-const words = str.split(' ');
-console.log(words[3]);
-const chars = str.split('');
-console.log(chars[8]);
-const strCopy = str.split();
-console.log(strCopy);
+let myFirstName = "Aryan"
+let myLastName = "aryan"
+console.log(myFirstName.startsWith("W"))
