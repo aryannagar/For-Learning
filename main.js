41abd1af6128e2ed8455127e47feee5c120e9f4e
Aryan Nagar <40473422+aryannagar@users.noreply.github.com>
2024-03-22 10:30:30 +0000

    String.charAt()

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 let myFirstName = "Aryan"
 let myLastName = "Nagar"
-/**
- * ! this is a test
- */
-/* ! Testing only */
-
-
-let myFullName = myFirstName.localeCompare(myLastName)
-console.log(myFullName)
+console.log(myFirstName.charAt(1))
