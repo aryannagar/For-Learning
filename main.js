cccc42ff7dfdb2059f83a19c0b478e6212893aff
Aryan Nagar <40473422+aryannagar@users.noreply.github.com>
2024-03-22 08:41:21 +0000

    NodeJS container creation.

---
+++ main.js
@@ -0,0 +1,10 @@
+let myFirstName = "Aryan"
+let myLastName = "Nagar"
+/**
+ * ! this is a test
+ */
+/* ! Testing only */
+
+
+let myFullName = myFirstName.localeCompare(myLastName)
+console.log(myFullName)
